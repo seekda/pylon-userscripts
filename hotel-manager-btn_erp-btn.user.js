becbc4b5404a6cb03dfbc39d2b42b041ad4981a7
mg-seekda <michael.guggenbichler@seekda.com>
2025-08-28 09:37:07 +0200

    Update hotel-manager-btn_erp-btn.user.js

--- hotel-manager-btn_erp-btn.user.js
+++ hotel-manager-btn_erp-btn.user.js
@@ -6,6 +6,8 @@
 // @match        https://app.usepylon.com/issues/*
 // @run-at       document-idle
 // @author       you
+// @updateURL    https://raw.githubusercontent.com/mg-seekda/pylon-userscripts/main/pylon-zitat.user.js
+// @downloadURL  https://raw.githubusercontent.com/mg-seekda/pylon-userscripts/main/pylon-zitat.user.js
 // @grant        GM_xmlhttpRequest
 // @connect      hotels.seekda.com
 // ==/UserScript==
